edea2a657079059f3a2260a70cb6df5fa41caf8b
JosipBartulovic <josip.bartulovic3@gmail.com>
2017-10-18 17:23:42 +0200

    Added 'veml6040' color sensor library

--- Programing/MARKOlb/MARKOlb.ino
+++ Programing/MARKOlb/MARKOlb.ino
@@ -1,6 +1,9 @@
 #include "lib.h"
+#include <Wire.h>
+#include "veml6040.h"
 
 void setup(){
+    Wire.begin();
     unsigned char niz[] = {1,2,3,4};
     unsigned char kol = 4;
     SensorArray senzor(niz, kol);
@@ -8,7 +11,8 @@ void setup(){
     for(int i=0; i<kol; i++){
       Serial.println(senzor.getValueArray()[i]);
     }
-
+    VEML6040 RGBWSensor;    
+    RGBWSensor.setConfiguration(VEML6040_IT_320MS + VEML6040_AF_AUTO + VEML6040_SD_ENABLE);
     MotorDrive drive(1,2,3,4,5,6,7);
 }
 
